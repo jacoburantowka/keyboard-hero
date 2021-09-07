3e9bbd4e0cbb0037049bdc5d42cff355857c27b4
Jacob <urantowka.jacob@gmail.com>
2021-09-08 00:06:04 +0100

    Initial project set up

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <KeyboardHero/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import KeyboardHero from "./views/KeyboardHero";
+
+export default {
+  name: 'App',
+
+  components: {
+    KeyboardHero,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/KeyboardHero.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-container class="keyboard">
+    <ul class="row row-0">
+      <li>ESC</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>0</li>
+      <li>-</li>
+      <li>+</li>
+      <li>BACK</li>
+    </ul>
+    <ul class="row row-1">
+      <li>TAB</li>
+      <li>Q</li>
+      <li>W</li>
+      <li>E</li>
+      <li>R</li>
+      <li>T</li>
+      <li>Y</li>
+      <li>U</li>
+      <li>I</li>
+      <li>O</li>
+      <li>P</li>
+      <li>[</li>
+      <li>]</li>
+      <li>\</li>
+    </ul>
+    <ul class="row row-2">
+      <li>CAPS</li>
+      <li>A</li>
+      <li>S</li>
+      <li>D</li>
+      <li>F</li>
+      <li>G</li>
+      <li>H</li>
+      <li>J</li>
+      <li>K</li>
+      <li>L</li>
+      <li>:</li>
+      <li>@</li>
+      <li>ENTER</li>
+    </ul>
+    <ul class="row row-3">
+      <li>SHIFT</li>
+      <li>Z</li>
+      <li>X</li>
+      <li>C</li>
+      <li>V</li>
+      <li>B</li>
+      <li>N</li>
+      <li>M</li>
+      <li>,</li>
+      <li>.</li>
+      <li>/</li>
+      <li>SHIFT</li>
+    </ul>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "KeyboardHero"
+}
+</script>
+
+<style scoped>
+
+</style>
