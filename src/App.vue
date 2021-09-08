90242f7d0f9d49c8cddb7614ce23b0c85a0cadc0
Jacob <urantowka.jacob@gmail.com>
2021-09-08 21:33:19 +0100

    Center keyboard on page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <v-app>
-    <v-main>
-      <KeyboardHero/>
-    </v-main>
-  </v-app>
+  <v-container>
+    <KeyboardHero/>
+  </v-container>
 </template>
 
 <script>
@@ -21,3 +19,16 @@ export default {
   }),
 };
 </script>
+
+<style>
+body {
+  background-color: rgba(0, 0, 0, 0.9);
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
--- src/views/KeyboardHero.vue
+++ src/views/KeyboardHero.vue
@@ -1,67 +1,67 @@
 <template>
-  <v-container class="keyboard">
-    <ul class="row row-0">
-      <li>ESC</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>0</li>
-      <li>-</li>
-      <li>+</li>
-      <li>BACK</li>
-    </ul>
-    <ul class="row row-1">
-      <li>TAB</li>
-      <li>Q</li>
-      <li>W</li>
-      <li>E</li>
-      <li>R</li>
-      <li>T</li>
-      <li>Y</li>
-      <li>U</li>
-      <li>I</li>
-      <li>O</li>
-      <li>P</li>
-      <li>[</li>
-      <li>]</li>
-      <li>\</li>
-    </ul>
-    <ul class="row row-2">
-      <li>CAPS</li>
-      <li>A</li>
-      <li>S</li>
-      <li>D</li>
-      <li>F</li>
-      <li>G</li>
-      <li>H</li>
-      <li>J</li>
-      <li>K</li>
-      <li>L</li>
-      <li>:</li>
-      <li>@</li>
-      <li>ENTER</li>
-    </ul>
-    <ul class="row row-3">
-      <li>SHIFT</li>
-      <li>Z</li>
-      <li>X</li>
-      <li>C</li>
-      <li>V</li>
-      <li>B</li>
-      <li>N</li>
-      <li>M</li>
-      <li>,</li>
-      <li>.</li>
-      <li>/</li>
-      <li>SHIFT</li>
-    </ul>
-  </v-container>
+    <v-container class="keyboard">
+      <ul class="row row-0">
+        <li class="pinky" id="esc">ESC</li>
+        <li class="pinky" id="1">1</li>
+        <li class="ring" id="2">2</li>
+        <li class="middle" id="3">3</li>
+        <li class="pointer" id="4">4</li>
+        <li class="pointer" id="5">5</li>
+        <li class="pointer" id="6">6</li>
+        <li class="pointer" id="7">7</li>
+        <li class="middle" id="8">8</li>
+        <li class="ring" id="9">9</li>
+        <li class="pinky" id="0">0</li>
+        <li class="pinky">-</li>
+        <li class="pinky">+</li>
+        <li class="pinky" id="back">BACK</li>
+      </ul>
+      <ul class="row row-1">
+        <li class="pinky" id="tab">TAB</li>
+        <li class="pinky" id="Q">Q</li>
+        <li class="ring" id="W">W</li>
+        <li class="middle" id="E">E</li>
+        <li class="pointer" id="R">R</li>
+        <li class="pointer" id="T">T</li>
+        <li class="pointer" id="Y">Y</li>
+        <li class="pointer" id="U">U</li>
+        <li class="middle" id="I">I</li>
+        <li class="ring" id="O">O</li>
+        <li class="pinky" id="P">P</li>
+        <li class="pinky">[</li>
+        <li class="pinky">]</li>
+        <li class="pinky">\</li>
+      </ul>
+      <ul class="row row-2">
+        <li class="pinky" id="caps">CAPS</li>
+        <li class="pinky" id="A">A</li>
+        <li class="ring" id="S">S</li>
+        <li class="middle" id="D">D</li>
+        <li class="pointer" id="F">F</li>
+        <li class="pointer" id="G">G</li>
+        <li class="pointer" id="H">H</li>
+        <li class="pointer" id="J">J</li>
+        <li class="middle" id="K">K</li>
+        <li class="ring" id="L">L</li>
+        <li class="pinky">:</li>
+        <li class="pinky">@</li>
+        <li class="pinky" id="enter">ENTER</li>
+      </ul>
+      <ul class="row row-3">
+        <li class="pinky" id="left-shit">SHIFT</li>
+        <li class="pinky" id="Z">Z</li>
+        <li class="ring" id="X">X</li>
+        <li class="middle" id="C">C</li>
+        <li class="pointer" id="V">V</li>
+        <li class="pointer" id="B">B</li>
+        <li class="pointer" id="N">N</li>
+        <li class="pointer" id="M">M</li>
+        <li class="middle">,</li>
+        <li class="ring">.</li>
+        <li class="pinky">/</li>
+        <li class="pinky" id="right-shift">SHIFT</li>
+      </ul>
+    </v-container>
 </template>
 
 <script>
@@ -71,5 +71,10 @@ export default {
 </script>
 
 <style scoped>
-
+.keyboard {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+}
 </style>
